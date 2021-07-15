82524d55f5c9c96b523f1c280c870e04e9fc0f0b
Mateja <mail@matejamaric.com>
2021-07-15 23:11:45 +0200

    Setup `siteUrl` environment variable.

--- server/config/env.js
+++ server/config/env.js
@@ -3,6 +3,7 @@ module.exports = {
   port: process.env.PORT || 3000,
   mongoUrl: process.env.DB_CONN || "mongodb://localhost/mevn_ecommerce_db",
   masterKey: process.env.APP_SECRET || "you-should-use-something-different",
+  siteUrl: process.env.SITE_URL || `http://localhost:${process.env.PORT || 3000}`,
   paypalEnvironment: process.env.PAYPAL_ENVIRONMENT || "sandbox",
   paypalClientId: process.env.PAYPAL_CLIENT_ID || 'PAYPAL-SANDBOX-CLIENT-ID',
   paypalClientSecret: process.env.PAYPAL_CLIENT_SECRET || 'PAYPAL-SANDBOX-CLIENT-SECRET'
--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -1,5 +1,6 @@
 const paypal = require('@paypal/checkout-server-sdk');
 const paypalClient = require('../config/paypal');
+const {siteUrl} = require('../config/env');
 
 module.exports = {
 
