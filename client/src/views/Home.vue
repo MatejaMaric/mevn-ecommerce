83b0f4515fae17278bcae1dfd25ee08109124bc6
Mateja <mail@matejamaric.com>
2021-07-24 20:46:13 +0200

    Center product cards.

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row mt-5">
+  <div class="row mt-5 d-flex justify-content-center">
     <div v-for="product in products" :key="product._id" class="col-lg-3 col-md-4 col-sm-6">
       <ProductCard :Id="product._id" :Name="product.name" :Price="product.price" :Img="product.imagePath"/>
     </div>
