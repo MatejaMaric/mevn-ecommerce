f847f7dd0f3728b9aab319a156c54a8cab388f0d
Mateja <mail@matejamaric.com>
2021-07-27 13:41:02 +0200

    Add Model component.

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="modal d-block" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ title }}</h5>
+          <button type="button" class="btn-close" @click="close"></button>
+        </div>
+        <div class="modal-body">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="modal-backdrop show"></div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    title: String,
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+    }
+  }
+}
+</script>
