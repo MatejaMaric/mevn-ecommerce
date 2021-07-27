10b0444eacee8e54c947a88b1cc27252666fe14c
Mateja <mail@matejamaric.com>
2021-07-27 19:16:14 +0200

    Protect API endpoint for showing paid orders.

--- server/routes/api.js
+++ server/routes/api.js
@@ -17,7 +17,7 @@ router.post('/products', isAuth, isAdmin, upload.single('image'), productsContro
 router.patch('/products/:id', isAuth, isAdmin, upload.single('image'), productsController.update);
 router.delete('/products/:id', isAuth, isAdmin, productsController.destroy);
 
-router.get('/transactions/paid', transactionController.showPaid);
+router.get('/transactions/paid', isAuth, isAdmin, transactionController.showPaid);
 router.post('/transactions/setup', transactionController.setup);
 router.post('/transactions/capture', transactionController.capture);
 
